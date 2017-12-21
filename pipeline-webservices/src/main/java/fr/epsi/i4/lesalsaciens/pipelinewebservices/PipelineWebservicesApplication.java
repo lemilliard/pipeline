489a812604b08e1ab2d7d78498b251c9a8c730b3
Thomas Kint <thomas.kint@epsi.fr>
2017-12-21 22:01:24 +0100

    Ajout du projet des webservices

---
+++ pipeline-webservices/src/main/java/fr/epsi/i4/lesalsaciens/pipelinewebservices/PipelineWebservicesApplication.java
@@ -0,0 +1,12 @@
+package fr.epsi.i4.lesalsaciens.pipelinewebservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PipelineWebservicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PipelineWebservicesApplication.class, args);
+	}
+}
