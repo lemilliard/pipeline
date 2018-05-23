2d476183b622b3627347e124bc96112e5efb290a
CGO34 <capdevillegeoffroy@gmail.com>
2018-05-23 10:45:53 +0200

    add organiser folder

---
+++ pipeline-front/pipeline-front-web/src/components/pages/organiser/Organiser.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="organiser">
+
+  </div>
+</template>
+
+<script>
+  import { mapState, mapActions } from 'vuex';
+  import DataActionsTypes from '@/store/data/actions/types';
+  import DataResourcesMap from '@/store/data/resources-map';
+  import DataResources from '@/store/data/resources';
+
+  export default {
+        name: 'Organiser',
+  };
+</script>
+
+<style scoped>
+
+</style>
