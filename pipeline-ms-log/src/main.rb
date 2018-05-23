254e2e759e3f61c3ccc26ef8affc8ce49e17e184
lbouv <ludovic.bouvier3@epsi.fr>
2018-05-23 12:46:01 +0200

    ws log

--- pipeline-ms-log/src/main.rb
+++ pipeline-ms-log/src/main.rb
@@ -1,5 +1,7 @@
 require 'sinatra'
 require 'sinatra/reloader'
+require 'json'
+require 'pp'
 
 set :bind, '0.0.0.0'
 set :port, 8080
@@ -8,12 +10,21 @@ get '/' do
   erb :'index.html'
 end
 
-get '/log' do
-  'coucou'
+post '/log' do
+  path = 'C:/Users/ludau/GoPath/src/github.com/pipeline/pipeline-ms-log/src/log/logs.txt'
+  values = JSON.parse(request.env["rack.input"].read)
+  begin
+    file = File.open(path, 'a')
+    file.puts(values.to_json) 
+  rescue IOError => e
+    #some error occur, dir not writable etc.
+  ensure
+    file.close unless file.nil?
+  end
 end
 
-get '/test' do
-  'ceci est un test'
+get '/log' do 
+    File.open('C:/Users/ludau/GoPath/src/github.com/pipeline/pipeline-ms-log/src/log/logs.txt', 'r')
 end
 
 get '/hello/:name' do
